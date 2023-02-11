19d6cdb0b157bf6582b23581d55eb73a58a6d8e6
AhadAli01 <ahadali101aa@gmail.com>
2023-02-11 15:41:57 -0700

    changed port

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 app.get('/', (req, res) => {
   res.send('This is the firebase endpoint');

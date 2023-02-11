f385e79182618feb0463700cc7b3a650b068b8c6
AhadAli01 <ahadali101aa@gmail.com>
2023-02-11 16:01:25 -0700

    setup

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 5001;
+const { users } = require('./models/users');
+
+app.get('/users', users);
 
 app.get('/', (req, res) => {
   res.send('This is the firebase endpoint');

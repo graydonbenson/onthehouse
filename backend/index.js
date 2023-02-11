807c18bd01d435fd60a62eaeba9b29c9b3f5888d
Mushtaba <MushtabaA1234@gmail.com>
2023-02-11 15:36:01 -0700

    add initial firebase setup & create endpoint

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+    res.send('This is the firebase endpoint');
+})
+
+app.listen(PORT, function () {
+    console.log(`Demo project at: ${PORT}!`);
+});

4a071c9f4f1ef7454aa63f3a2e8f33192eb4d00d
Athul Rajagopal <49791750+rajagoat@users.noreply.github.com>
2023-02-11 16:04:13 -0700

    signup and login redirects

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -12,6 +12,7 @@ import Grid from "@mui/material/Grid";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import nextLink from "next/link";
 
 function Copyright(props) {
   return (
@@ -124,7 +125,7 @@ export default function SignInSide() {
                   </Link>
                 </Grid>
                 <Grid item>
-                  <Link href="#" variant="body2">
+                  <Link href="/signup" variant="body2">
                     {"Don't have an account? Sign Up"}
                   </Link>
                 </Grid>

e71074a63e71b863e461204dcc4daa3017c03975
Mushtaba <MushtabaA1234@gmail.com>
2023-02-13 15:37:38 -0700

    Create get users endpoint

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,9 +1,22 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+let db = admin.firestore();
 
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+exports.helloWorld = functions.https.onRequest((req, res) => {
+  res.send("Hello from Firebase!");
+});
+
+exports.getUsers = functions.https.onRequest((req, res) => {
+    let users = [];
+    db.collection("Users").get().then((querySnap) => {
+        querySnap.forEach((doc) => {
+            users.push(doc.data());
+        });
+        return res.json(users);
+    }).catch(error => console.error(error));
 });

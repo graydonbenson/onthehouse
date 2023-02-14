539e173c0b95d6b4e95be74932ef0e77a18b363a
Mushtaba <MushtabaA1234@gmail.com>
2023-02-13 19:14:34 -0700

    Create basic pages, and add routes using react-router

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+
+function App() {
+  return (
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage/>} />
+          <Route path="/login" element={<LoginPage/>} />
+          <Route path="/signup" element={<SignUpPage/>} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;

f4180dee7b8395266ceb20094414fa8fb2beca96
Mushtaba <MushtabaA1234@gmail.com>
2023-03-18 16:00:20 -0600

    update backend login/signup error handling

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,32 +1,32 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const cors = require('cors');
-const { initializeApp } = require('firebase/app');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const cors = require("cors");
+const { initializeApp } = require("firebase/app");
 const {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   sendPasswordResetEmail,
   signOut,
-} = require('firebase/auth');
+} = require("firebase/auth");
 
-const { FieldValue } = require('firebase-admin/firestore');
+const { FieldValue } = require("firebase-admin/firestore");
 
 admin.initializeApp();
 const db = admin.firestore();
 
-const express = require('express');
+const express = require("express");
 const app = express();
 app.use(cors());
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAZToVDJJDQle7Z6Q-fevMs9CF-xWZfbFw',
-  authDomain: 'seng-401-on-the-house.firebaseapp.com',
-  projectId: 'seng-401-on-the-house',
-  storageBucket: 'seng-401-on-the-house.appspot.com',
-  messagingSenderId: '439573630964',
-  appId: '1:439573630964:web:98ca7406811fc583fef3a1',
-  measurementId: 'G-EFH2Y2NN1X',
+  apiKey: "AIzaSyAZToVDJJDQle7Z6Q-fevMs9CF-xWZfbFw",
+  authDomain: "seng-401-on-the-house.firebaseapp.com",
+  projectId: "seng-401-on-the-house",
+  storageBucket: "seng-401-on-the-house.appspot.com",
+  messagingSenderId: "439573630964",
+  appId: "1:439573630964:web:98ca7406811fc583fef3a1",
+  measurementId: "G-EFH2Y2NN1X",
 };
 
 initializeApp(firebaseConfig);
@@ -36,9 +36,9 @@ initializeApp(firebaseConfig);
 
 // ! Users Endpoints
 // GET /users - get all users (from firestore)
-app.get('/users', (req, res) => {
+app.get("/users", (req, res) => {
   let users = [];
-  db.collection('Users')
+  db.collection("Users")
     .get()
     .then((querySnap) => {
       querySnap.forEach((doc) => {
@@ -51,13 +51,20 @@ app.get('/users', (req, res) => {
 
 // for frontend if /signup api request is successful, redirect to login page
 // POST /signup - add user (to firebase auth and firestore)
-app.post('/signup', async (req, res) => {
+app.post("/signup", async (req, res) => {
   const registerUser = {
     fullName: req.body.fullName,
     email: req.body.email,
     username: req.body.username,
     password: req.body.password,
   };
+
+  const usernameRef = await db.collection("Users").doc(registerUser.username);
+  const doc = await usernameRef.get();
+
+  if (doc.exists) {
+    res.send({code: "Username already taken!"});
+  }
 
   try {
     const auth = await getAuth();
@@ -67,49 +74,26 @@ app.post('/signup', async (req, res) => {
       registerUser.password
     );
 
-    // Check if username exists
-    const usernameRef = await db.collection('Users').doc(registerUser.username);
-
-    const doc = await usernameRef.get();
-    if (!doc.exists) {
-      console.log('No such document!');
-
-      await db
-        .collection('Users')
-        .doc(registerUser.username)
-        //   .doc(userCredentials.user.uid)
-        .set(
-          {
-            fullName: registerUser.fullName,
-            email: registerUser.email,
-            username: registerUser.username,
-          },
-          { merge: false }
-        )
-        .then(() => {
-          console.log('User document successfully created!');
-          res.send({ message: 'Created User Successfully' });
-        })
-        .catch((error) => {
-          res.status(404);
-          console.error(error);
-          res.send(error);
-        });
-    } else {
-      res.status(404);
-      console.log('Document data:', doc.data());
-      res.send({ message: 'Username already taken.' });
-    }
+    await db.collection("Users").doc(registerUser.username).set(
+        {
+          fullName: registerUser.fullName,
+          email: registerUser.email,
+          username: registerUser.username,
+        },
+        { merge: false }
+      ).then(() => {
+        console.log("User document successfully created!");
+      }).catch((error) => {
+        res.send(error);
+      });
   } catch (error) {
-    res.status(404);
-    console.error(error);
-    res.send(error); // for frontend - if error sent/check status code is not 200 then alert(error.message)
+    res.send(error);
   }
 });
 
 // for frontend, if successful login save returned user object (or just email) in session/browser Storage
 // GET /login - get user (from firebase auth) given user's email and password parameters
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   const loginUser = {
     email: req.body.email,
     password: req.body.password,
@@ -123,34 +107,30 @@ app.post('/login', async (req, res) => {
       loginUser.password
     );
     // Get usercredentials from firestore
-    const userEmailRef = await db.collection('Users');
+    const userEmailRef = await db.collection("Users");
     //   .doc(registerUser.username)
     // .doc(userCredentials.user.uid)
     const snapshot = await userEmailRef
-      .where('email', '==', loginUser.email)
+      .where("email", "==", loginUser.email)
       .get();
 
     if (snapshot.empty) {
       res.status(404);
-      console.log('No matching documents.');
-      res.send({ message: 'No matching documents!' });
+      console.log("No matching documents.");
+      res.send({ message: "No matching documents!" });
     }
 
     snapshot.forEach((doc) => {
-      console.log(doc.id, '=>', doc.data());
+      console.log(doc.id, "=>", doc.data());
       res.send({ ...doc.data(), ...userCredentials });
     });
-
-    // res.send(userCredentials);
   } catch (error) {
-    res.status(404);
-    console.error(error);
     res.send(error); // for frontend - if error sent/check status code is not 200 then alert(error.message)
   }
 });
 
 // POST /resetPassword - send user a password reset link to their email address
-app.post('/resetPassword', async (req, res) => {
+app.post("/resetPassword", async (req, res) => {
   try {
     const auth = getAuth();
     await sendPasswordResetEmail(auth, req.body.email);
@@ -162,11 +142,11 @@ app.post('/resetPassword', async (req, res) => {
 });
 
 // POST /logout - signout user
-app.post('/logout', async (req, res) => {
+app.post("/logout", async (req, res) => {
   try {
     const auth = getAuth();
     signOut(auth);
-    res.send({ message: 'Successfully logged out user' });
+    res.send({ message: "Successfully logged out user" });
   } catch (error) {
     console.error(error);
     res.send(error); // for frontend - if error sent/check status code is not 200 then alert(error.message)
@@ -175,9 +155,9 @@ app.post('/logout', async (req, res) => {
 
 // !Posts
 // GET /posts - get all posts
-app.get('/posts', (req, res) => {
+app.get("/posts", (req, res) => {
   let posts = [];
-  db.collection('Posts')
+  db.collection("Posts")
     .get()
     .then((querySnap) => {
       querySnap.forEach((doc) => {
@@ -196,16 +176,16 @@ app.get('/posts', (req, res) => {
 });
 
 // GET /posts/:id - get post by post id
-app.get('/posts/:id', (req, res) => {
-  db.collection('Posts')
+app.get("/posts/:id", (req, res) => {
+  db.collection("Posts")
     .doc(req.params.id)
     .get()
     .then((doc) => {
       if (doc.exists) {
         let comments = [];
-        db.collection('Posts')
+        db.collection("Posts")
           .doc(doc.id)
-          .collection('Comments')
+          .collection("Comments")
           .get()
           .then((querySnap) => {
             querySnap.forEach((doc) => {
@@ -220,8 +200,8 @@ app.get('/posts/:id', (req, res) => {
             res.send(error);
           });
       } else {
-        console.log('No such document!');
-        res.send({ message: 'No such document!' });
+        console.log("No such document!");
+        res.send({ message: "No such document!" });
       }
     })
     .catch((error) => {
@@ -231,7 +211,7 @@ app.get('/posts/:id', (req, res) => {
 });
 
 // POST /posts - create a new post
-app.post('/posts', (req, res) => {
+app.post("/posts", (req, res) => {
   const newPost = {
     title: req.body.title,
     description: req.body.description,
@@ -243,12 +223,12 @@ app.post('/posts', (req, res) => {
     date: new Date(),
   };
 
-  db.collection('Posts')
+  db.collection("Posts")
     .doc()
     .set(newPost)
     .then(() => {
-      console.log('Post document successfully created!');
-      res.send({ message: 'Created Post Successfully' });
+      console.log("Post document successfully created!");
+      res.send({ message: "Created Post Successfully" });
     })
     .catch((error) => {
       console.error(error);
@@ -257,7 +237,7 @@ app.post('/posts', (req, res) => {
 });
 
 // POST /upvotes - upvote a post
-app.post('/upvotes', (req, res) => {
+app.post("/upvotes", (req, res) => {
   const newUpvote = {
     postId: req.body.postId,
     userId: req.body.userId,
@@ -275,22 +255,22 @@ else:
   create a new record
 */
 
-  db.collection('Upvotes')
+  db.collection("Upvotes")
     .doc(docId)
     .get()
     .then((doc) => {
       if (doc.exists) {
         if (doc.data().isUpvote === newUpvote.isUpvote) {
-          const message = 'Post has already been upvoted/downvoted by user.';
+          const message = "Post has already been upvoted/downvoted by user.";
           console.log(message);
           res.send({ message: message });
         } else {
           // Update isUpvote in found record
-          db.collection('Upvotes')
+          db.collection("Upvotes")
             .doc(docId)
             .update({ isUpvote: newUpvote.isUpvote })
             .then(() => {
-              console.log('IsUpvote attribute updated!');
+              console.log("IsUpvote attribute updated!");
             })
             .catch((error) => {
               console.error(error);
@@ -311,7 +291,7 @@ else:
 			*/
 
           // Update upvoteCount in posts
-          db.collection('Posts')
+          db.collection("Posts")
             .doc(newUpvote.postId)
             .update({
               upvoteCount: newUpvote.isUpvote
@@ -319,8 +299,8 @@ else:
                 : FieldValue.increment(-2),
             })
             .then(() => {
-              console.log('upvoteCount attribute updated!');
-              res.send({ message: 'Upvoted/Downvoted Post Successfully' });
+              console.log("upvoteCount attribute updated!");
+              res.send({ message: "Upvoted/Downvoted Post Successfully" });
             })
             .catch((error) => {
               console.error(error);
@@ -329,11 +309,11 @@ else:
         }
       } else {
         // Create a new record
-        db.collection('Upvotes')
+        db.collection("Upvotes")
           .doc(docId)
           .set(newUpvote)
           .then(() => {
-            console.log('Upvote document successfully created!');
+            console.log("Upvote document successfully created!");
           })
           .catch((error) => {
             console.error(error);
@@ -341,7 +321,7 @@ else:
           });
 
         // Update upvoteCount in posts
-        db.collection('Posts')
+        db.collection("Posts")
           .doc(newUpvote.postId)
           .update({
             upvoteCount: newUpvote.isUpvote
@@ -349,8 +329,8 @@ else:
               : FieldValue.increment(-1),
           })
           .then(() => {
-            console.log('upvoteCount attribute updated!');
-            res.send({ message: 'Upvoted/Downvoted Post Successfully' });
+            console.log("upvoteCount attribute updated!");
+            res.send({ message: "Upvoted/Downvoted Post Successfully" });
           })
           .catch((error) => {
             console.error(error);
@@ -366,21 +346,21 @@ else:
 
 // Comments
 // POST /comments/:postId - add a comment to a specific post
-app.post('/comments/:postId', async (req, res) => {
+app.post("/comments/:postId", async (req, res) => {
   const newComment = {
     message: req.body.message,
     userId: req.body.userId,
   };
 
   await db
-    .collection('Posts')
+    .collection("Posts")
     .doc(req.params.postId)
-    .collection('Comments')
+    .collection("Comments")
     .doc()
     .set(newComment)
     .then(() => {
-      console.log('Comment document successfully created!');
-      res.send({ message: 'Created Comment Successfully' });
+      console.log("Comment document successfully created!");
+      res.send({ message: "Created Comment Successfully" });
     })
     .catch((error) => {
       console.error(error);
@@ -391,12 +371,12 @@ app.post('/comments/:postId', async (req, res) => {
 // !Helper Function
 async function getuserNamebyUserId(userId) {
   await db
-    .collection('Users')
+    .collection("Users")
     .doc(userId)
     .get()
     .then((doc) => {
       const username = doc.data().username;
-      console.log('Document data:', username);
+      console.log("Document data:", username);
       return { username };
     })
     .catch((error) => {

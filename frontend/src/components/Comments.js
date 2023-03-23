c7d2a0fdb488fde0fb4e3dcdff548915255a6dce
AhadAli01 <ahadali101aa@gmail.com>
2023-03-23 13:33:18 -0600

    fix/finalize comments

--- frontend/src/components/Comments.js
+++ frontend/src/components/Comments.js
@@ -5,33 +5,40 @@ import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
 
-export default function Comments() {
-  const params = useParams();
+export default function Comments({ postId }) {
   const [postComments, setPostComments] = useState([]);
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
     const fetchPost = async () => {
-      const response = await fetch(`/posts/${params.id}`);
+      const response = await fetch(
+        `${process.env.REACT_APP_API_URL}/comments/${postId}`
+      );
       const json = await response.json();
-      const com = json.comments;
-      setPostComments(com.map(comments => comments.message));
-      setUsers(com.map(comments => comments.userId));
-    }
+      console.log(json);
+      setPostComments(json.comments.map((c) => c.message));
+      setUsers(json.comments.map((c) => c.userId));
+    };
 
     fetchPost();
-  }, [params.id]);
+  }, [postId]);
 
   return (
-    <List sx={{ width: '100%', maxWidth: 450, maxHeight: 700, bgcolor: 'aliceblue' }}>
+    <List
+      sx={{
+        width: '100%',
+        maxWidth: 450,
+        maxHeight: 700,
+        bgcolor: 'aliceblue',
+      }}
+    >
       {postComments.map((comment, index) => (
         <ListItem alignItems="flex-start">
           <ListItemAvatar>
             <Avatar alt={users[index]} src="/static/images/avatar/3.jpg" />
           </ListItemAvatar>
-          <ListItemText style={{ transform:'translateY(50%)' }}>
+          <ListItemText style={{ transform: 'translateY(50%)' }}>
             {users[index]}: {comment}
           </ListItemText>
         </ListItem>

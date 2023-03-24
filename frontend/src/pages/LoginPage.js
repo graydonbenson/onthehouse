4fb973ffbea3f36ec4c65eabdd49c4bed6380843
Athul Rajagopal <49791750+rajagoat@users.noreply.github.com>
2023-03-24 02:53:51 -0600

    login page has image

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -8,12 +8,14 @@ import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import axios from 'axios';
 import { Navigate } from "react-router-dom";
@@ -107,101 +109,138 @@ export default function SignIn() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Container
-        component="main"
-        maxWidth="xs"
-        sx={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center'
-        }}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
         <CssBaseline />
-        <Box
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
           sx={{
+            backgroundImage: `url(${process.env.PUBLIC_URL}/login-image.jpg)`,
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid
+          item
+          xs={12}
+          sm={8}
+          md={5}
+          component={Paper}
+          elevation={6}
+          square
+          sx={{
+            height: '100vh',
             display: 'flex',
             flexDirection: 'column',
-            alignItems: 'center',
+            backgroundColor: '#fee7e7'
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              onChange={(event) => {
-                setSignInEmail(event.target.value);
-              }}
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              onChange={(event) => {
-                setSignInPassword(event.target.value);
-              }}
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            {isLoading ? (<><LinearProgress color='secondary' />
-              <Button
-                type="submit"
+          <Card sx={{ borderRadius: "0px", backgroundColor: '#f4bd7b' }}>
+            <CardHeader title={<Typography variant="h4" sx={{ fontFamily: "unset" }}>On The House 🍜</Typography>} />
+          </Card>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              flexGrow: 1,
+              justifyContent: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1, width: '100%' }}>
+              <TextField
+                margin="normal"
+                required
                 fullWidth
-                variant="contained"
-                disabled={true}
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign In
-              </Button></>) : (<>
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                onChange={(event) => {
+                  setSignInEmail(event.target.value);
+                }}
+                sx={{
+                  backgroundColor: 'white',
+                  borderRadius: 1
+                }}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={(event) => {
+                  setSignInPassword(event.target.value);
+                }}
+                sx={{
+                  backgroundColor: 'white',
+                  borderRadius: 1
+                }}
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              {isLoading ? (<><LinearProgress color='secondary' />
                 <Button
                   type="submit"
                   fullWidth
                   variant="contained"
+                  disabled={true}
                   sx={{ mt: 3, mb: 2 }}
                 >
                   Sign In
-                </Button></>)}
-            <Grid container>
-              <Grid item xs>
-                <Link href="forgotpassword" variant="body2">
-                  Forgot password?
-                </Link>
+                </Button></>) : (<>
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                  >
+                    Sign In
+                  </Button></>)}
+              <Grid container>
+                <Grid item xs>
+                  <Link href="forgotpassword" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="signup" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
               </Grid>
-              <Grid item>
-                <Link href="signup" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+            <Stack spacing={2} sx={{ width: '100%' }}>
+              <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+                  {errorMessage}
+                </Alert>
+              </Snackbar>
+            </Stack>
           </Box>
-        </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
-      <Stack spacing={2} sx={{ width: '100%' }}>
-        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-          <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
-            {errorMessage}
-          </Alert>
-        </Snackbar>
-      </Stack>
-    </ThemeProvider>
+        </Grid>
+      </Grid>
+    </ThemeProvider >
   );
 }
